6b52199c5bf75811e54e3c41d0db3d55df413d76
Ryan Harris <ryan.t.harris67@gmail.com>
2022-07-27 09:50:32 -0500

    updated notes & exercises

---
+++ js/external.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log('Hello from external Javascript');
+
+alert('Welcome to my website.');
+
+var userInput = prompt('What is your favorite color?');
+alert(userInput + ', is my favorite color as well!')
