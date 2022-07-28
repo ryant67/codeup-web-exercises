4bea42583d53d64326e59321b5c9c73df9f4cb5c
Ryan Harris <ryan.t.harris67@gmail.com>
2022-07-27 22:37:27 -0500

    Updated exercises

---
+++ html_file/js/external.js
@@ -0,0 +1,32 @@
+"use strict";
+
+console.log('Hello from external Javascript');
+
+alert('Welcome to my website.');
+
+var userInput = prompt('What is your favorite color?');
+alert(userInput + ', is my favorite color as well!')
+
+// * Exercise #3 * //
+
+var dailycost = 3;
+var mov1 = 3;
+var mov2 = 5;
+var mov3 = 1;
+var total = (mov1 * dailycost) + (mov2 * dailycost) + (mov3 * dailycost);
+
+var gp = 400 * 6;
+var ap = 380 * 4;
+var fp = 350 * 10;
+var totPay = gp + ap + fp;
+
+var isFull = !=true;
+var isConflict = !=true;
+let enrollment = true; {
+    if isFull = false && isConflict = false;
+}
+
+
+
+
+
