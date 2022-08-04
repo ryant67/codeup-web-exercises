3ad2447a4b3ac2ed4335793750ed51a0c4f98c36
Ryan Harris <ryan.t.harris67@gmail.com>
2022-08-04 14:10:45 -0500

    objects exercise added

--- html_file/js/planets-array.js
+++ html_file/js/planets-array.js
@@ -35,8 +35,7 @@
     console.log(planets);
     //
     // console.log('Finding and logging the index of "Earth" in the planets array.');
-    var earth = planets.indexOf('Earth');
-    console.log(earth);
+    console.log(planets.indexOf('Earth'));
     //
     // console.log("Reversing the order of the planets array.");
     planets.reverse();
